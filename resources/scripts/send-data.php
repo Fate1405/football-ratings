976be69c072fac2fbe9a62aae54543c175389db0
Fate1405 <58146913+Fate1405@users.noreply.github.com>
2021-12-28 18:00:22 +0100

    im an idiot

--- resources/scripts/send-data.php
+++ resources/scripts/send-data.php
@@ -16,10 +16,27 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "UPDATE player_ratings SET {$data[4]} = {$data[0]}, Appearances = {$data[2]} WHERE Player = {$data[5]}";
-$conn->query($sql);
+$sql = "UPDATE player_ratings SET {$data[4]} = {$data[0]}, Appearances = {$data[2]} WHERE Player = '{$data[5]}'";
+if ($result = $conn->query($sql)) {
+    echo $result;
+} else {
+    echo "Failure";
+}
 
-$sql = "UPDATE player_ratings SET {$data[4]} = {$data[1]}, Appearances = {$data[3]} WHERE Player = {$data[6]}";
-$conn->query($sql);
+$sql = "UPDATE player_ratings SET {$data[4]} = {$data[1]}, Appearances = {$data[3]} WHERE Player = '{$data[6]}'";
+if ($result = $conn->query($sql)) {
+    echo $result;
+} else {
+    echo "Failure";
+}
+
+
+echo "Should work... {$data[0]}";
+echo "Should work... {$data[1]}";
+echo "Should work... {$data[2]}";
+echo "Should work... {$data[3]}";
+echo "Should work... {$data[4]}";
+echo "Should work... {$data[5]}";
+echo "Should work... {$data[6]}";
 
 ?>
